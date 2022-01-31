4834b18ddb4bac75da2807971cf59652874be9e3
vmeucci <viniciusmeuci@icloud.com>
2022-01-30 22:13:54 -0300

    Updating the archives

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Enter contract data: ");
+		System.out.print("Number: ");
+		System.out.print("Date (dd/MM/yyyy): ");
+		System.out.print("Contract value: $");
+		System.out.println("");
+		System.out.print("Enter the number of installaments: ");
+		System.out.println("Installaments: ");
+		
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/model/entities/Contract.java
@@ -0,0 +1,24 @@
+package model.entities;
+
+public class Contract {
+	
+	private Integer number;
+	
+	public Contract() {
+	}
+
+	public Contract(Integer number) {
+		this.number = number;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+	
+	
+
+}
